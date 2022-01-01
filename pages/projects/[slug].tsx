edda30dd48132f42a9191b9f29b13da75acaa9ab
memwangi <donmwas007@gmail.com>
2022-01-01 21:30:30 +0300

    Updates

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,13 +1,12 @@
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
-import ErrorPage from 'next/error'
-import {Post} from "../../components/post";
+import ErrorPage from "next/error";
+import { Post } from "../../components/post";
 import { ParsedUrlQuery } from "querystring";
-import {useRouter} from 'next/router'
-import { getClient } from "../../lib/sanity.server";
+import { useRouter } from "next/router";
+import { getClient, sanityClient } from "../../lib/sanity.server";
 import { groq } from "next-sanity";
 import { usePreviewSubscription } from "../../lib/sanity";
 import { get } from "https";
-
 
 const ReactDOM = require("react-dom");
 const BlockContent = require("@sanity/block-content-to-react");
@@ -27,53 +26,35 @@ const postQuery = groq`*[_type == "post" && slug.current ==$slug][0] {
   }`;
 
 export const Project = ({
-	data, preview
+	data,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-	const router =  useRouter()
+	const router = useRouter();
 
-	const {data: post} = usePreviewSubscription(postQuery, {
-		params: {slug: data.post?.slug},
-		initialData: data.post,
-		enabled: preview && data.post?.slug,
-	  })
-
-	if(!router.isFallback && !data.post?.slug) {
-		return <ErrorPage statusCode={404} />
+	if (!router.isFallback && !data.post?.slug) {
+		return <ErrorPage statusCode={404} />;
 	}
-	
-	return (
-		<Post project={post}/>
-	);
+
+	return <Post project={data?.post} />;
 };
 
-
-
 export const getStaticProps: GetStaticProps = async (context) => {
-	const preview = context.preview
 	const { slug = " " } = context.params as ParsedUrlQuery;
-	const post = await getClient(preview).fetch(postQuery, { slug });
-
-	
+	const post = await getClient().fetch(postQuery, { slug });
 
 	return {
 		props: {
-			preview,
-			data: {post},
+			data: { post },
 		},
 	};
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
 	const paths = await getClient().fetch(
-		`*[_type == "post" && defined(slug.current)] {
-			"params": {
-				"slug": slug.current
-			}
-		}`
+		`*[_type == "post" && defined(slug.current)][].slug.current`
 	);
 
 	return {
-		paths,
+		paths: paths.map((slug: any) => ({ params: { slug } })),
 		fallback: true,
 	};
 };
